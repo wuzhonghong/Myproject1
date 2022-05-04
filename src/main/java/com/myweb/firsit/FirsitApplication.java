b2789dd1e4973b5eaee1d6d10dfdcc328b200e15
wuzhonghong <367076223@qq.com>
2022-05-04 17:26:42 +0800

    首次提交

---
+++ src/main/java/com/myweb/firsit/FirsitApplication.java
@@ -0,0 +1,13 @@
+package com.myweb.firsit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirsitApplication.class, args);
+    }
+
+}
