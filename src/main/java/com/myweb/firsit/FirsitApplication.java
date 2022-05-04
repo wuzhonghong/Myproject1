29e370515c89cbca06160faf326fbf12e35c9733
wuzhonghong <367076223@qq.com>
2022-05-04 20:53:23 +0800

    优化日志与启动

--- src/main/java/com/myweb/firsit/FirsitApplication.java
+++ src/main/java/com/myweb/firsit/FirsitApplication.java
@@ -1,13 +1,28 @@
 package com.myweb.firsit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+// @ComponentScan({"com.jiawa", "com.test"})
+@ComponentScan("com.myweb")
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class FirsitApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FirsitApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(FirsitApplication.class, args);
+        SpringApplication app = new SpringApplication(FirsitApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
