06ff505b60e490a205fd33a0a79cdffe72a39f96
wuzhonghong <367076223@qq.com>
2022-05-06 00:20:20 +0800

    封面小图样式

--- src/main/java/com/myweb/firsit/service/EbookService.java
+++ src/main/java/com/myweb/firsit/service/EbookService.java
@@ -11,6 +11,7 @@ import com.myweb.firsit.resp.EbookResp;
 import com.myweb.firsit.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -23,7 +24,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%"+req.getName()+"%");
+        if(!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
 //        List<EbookResp> respList =new ArrayList<>();
